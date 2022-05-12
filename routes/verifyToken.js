24529a935dcc219311afccec487a185d88c0faf2
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-12 20:29:17 +0700

    splitting authheader

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,6 +3,7 @@ var jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
+    const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) {
         res.status(401).json({ message: 'Token is not valid!' });
