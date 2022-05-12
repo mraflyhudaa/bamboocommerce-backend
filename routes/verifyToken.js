8c1ddcec3fecff891752cb416bf06b60223d2123
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-12 20:05:06 +0700

    create token authorization  function

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -15,4 +15,14 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-module.exports = { verifyToken };
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json({ message: 'You are not allowed to do that!' });
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
