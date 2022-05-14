8864fe10b91208b25b2dab30564fd4dfe1103d3c
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-14 15:53:35 +0700

    change dimension and price data type to Array

--- models/Product.js
+++ models/Product.js
@@ -6,8 +6,8 @@ const ProductSchema = new mongoose.Schema(
     desc: { type: String, required: true, unique: true },
     img: { type: String, required: true },
     categories: { type: Array },
-    dimension: { type: String },
-    price: { type: Number, required: true },
+    dimension: { type: Array },
+    price: { type: Array, required: true },
   },
   { timestamps: true }
 );
