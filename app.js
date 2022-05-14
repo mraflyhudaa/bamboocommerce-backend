426ad180ab618b1dd9669dcb1c8657c8aaaecd18
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-14 16:35:39 +0700

    mistype app.use function

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
-app.user('/api/products', productRouter);
+app.use('/api/products', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
