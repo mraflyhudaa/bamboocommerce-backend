bb24a2d66a8e46b813a1dffe7acdd92368007119
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-14 16:36:32 +0700

    add CRUD to products route

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,108 @@
 var express = require('express');
+const Product = require('../models/Product');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require('./verifyToken');
+
+/* POST create product */
+router.post('/', verifyTokenAndAdmin, async (req, res, next) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json({ message: 'Product created!', savedProduct });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to create product', error });
+  }
 });
+
+/* PUT update product data */
+router.put('/:id', verifyTokenAndAdmin, async (req, res, next) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json({ message: 'Product updated!', data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to update product!', error });
+  }
+});
+
+/* DELETE delete product data */
+router.delete('/:id', verifyTokenAndAdmin, async (req, res, next) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    const { title } = deletedProduct._doc;
+    res.status(200).json({ message: `Product ${title} has been deleted!` });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete Product!', error });
+  }
+});
+
+/* GET product data */
+router.get('/find/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json({ message: 'Product found!', data: product });
+  } catch (error) {
+    res.status(500).json({ message: 'Product not found!', error });
+  }
+});
+
+/* GET all products */
+router.get('/', verifyTokenAndAdmin, async (req, res, next) => {
+  const queryNew = req.query.new;
+  const queryCategory = req.query.category;
+  try {
+    let products;
+
+    if (queryNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (queryCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [queryCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+    res.status(200).json({ message: 'Products found!', data: products });
+  } catch (error) {
+    res.status(500).json({ message: 'Products not found!', error });
+  }
+});
+
+// /* GET user stats */
+// router.get('/stats', verifyTokenAndAdmin, async (req, res, next) => {
+//   const date = new Date();
+//   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+//   try {
+//     const data = await User.aggregate([
+//       { $match: { createdAt: { $gte: lastYear } } },
+//       {
+//         $project: {
+//           month: { $month: '$createdAt' },
+//         },
+//       },
+//       {
+//         $group: {
+//           _id: '$month',
+//           total: { $sum: 1 },
+//         },
+//       },
+//     ]);
+//     res.status(200).json(data);
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
 
 module.exports = router;
