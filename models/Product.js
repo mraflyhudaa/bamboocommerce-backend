faa471ad004cdb603784162ffae7367be0963ca6
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-14 16:36:03 +0700

    set desc as not unique

--- models/Product.js
+++ models/Product.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array },
     dimension: { type: Array },
+    color: { type: String },
     price: { type: Array, required: true },
   },
   { timestamps: true }
