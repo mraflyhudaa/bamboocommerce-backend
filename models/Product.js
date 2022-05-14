ccec70fcc04a66ee59aca85c28d74aa2813718b7
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-14 15:35:49 +0700

    delete color model from product schema

--- models/Product.js
+++ models/Product.js
@@ -7,7 +7,6 @@ const ProductSchema = new mongoose.Schema(
     img: { type: String, required: true },
     categories: { type: Array },
     dimension: { type: String },
-    color: { type: String },
     price: { type: Number, required: true },
   },
   { timestamps: true }
