ba2078749eaff3f20efdcb4008bb7c798c30a5b5
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-15 19:05:05 +0700

    adding CRUD function to products route

--- routes/products.js
+++ routes/products.js
@@ -79,30 +79,4 @@ router.get('/', verifyTokenAndAdmin, async (req, res, next) => {
   }
 });
 
-// /* GET user stats */
-// router.get('/stats', verifyTokenAndAdmin, async (req, res, next) => {
-//   const date = new Date();
-//   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-//   try {
-//     const data = await User.aggregate([
-//       { $match: { createdAt: { $gte: lastYear } } },
-//       {
-//         $project: {
-//           month: { $month: '$createdAt' },
-//         },
-//       },
-//       {
-//         $group: {
-//           _id: '$month',
-//           total: { $sum: 1 },
-//         },
-//       },
-//     ]);
-//     res.status(200).json(data);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
-
 module.exports = router;
