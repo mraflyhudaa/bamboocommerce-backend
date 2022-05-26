40841a04447fd6ff3e7d11f9cd3813fbc8402d3e
mraflyhudaa <mraflyhudaa@gmail.com>
2022-05-26 13:06:01 +0700

    create dbConfig to initialize sequelize

---
+++ config/dbConfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: 'localhost',
+  USER: 'root',
+  PASSWORD: 'Rootq1e3!!',
+  DB: 'dekorindo_ecommerce',
+  DIALECT: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
