7491dcfacc4ca0c6baad362853d4948dc3798d02
Taufik1606 <taufikth1606@gmail.com>
2022-07-13 23:13:11 +0700

    set unique key to orderId

--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema(
   {
-    orderId: { type: String, required: true },
+    orderId: { type: String, required: true, unique: true },
     userId: { type: String, required: true },
     name: { type: String },
     products: [
